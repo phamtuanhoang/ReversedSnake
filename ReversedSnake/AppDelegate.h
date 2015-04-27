cc6eeefb6e23747ffe3a4da53d32527aba121eca
hoangpham <hoangpham1107@gmail.com>
2015-04-27 23:46:04 +0800

    Initial Commit

---
+++ ReversedSnake/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReversedSnake
+//
+//  Created by hoangpham on 27/4/15.
+//  Copyright (c) 2015 hoangpham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
